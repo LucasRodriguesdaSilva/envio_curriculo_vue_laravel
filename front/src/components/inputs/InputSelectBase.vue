3a184268838bbb097dd2a5028bcb389124943bd8
Lucas Rodrigues <lucasrodrigues@sobral.ce.gov.br>
2024-05-14 13:34:19 -0300

    feat: Adicionado componente de select

---
+++ front/src/components/inputs/InputSelectBase.vue
@@ -0,0 +1,30 @@
+<template>
+    <label for="id" class="form-label">Escolariadade</label>
+    <select id="id" @change="atualizarSelecionado" :required="obrigatorio">
+        <option value="" selected disabled>Selecione uma Opção</option>
+        <option v-for="opcao in opcoes" :key="opcao.id" :value="opcao.id" v-bind="$attrs">
+            {{ opcao.valor }}
+        </option>
+    </select>
+</template>
+
+<script setup>
+    import { ref } from 'vue';
+    const selecionado = ref('');
+    defineProps({
+        opcoes: Array,
+        obrigatorio: { type: Boolean, default: false},
+
+    })
+    
+    const emit = defineEmits(['update:selecionado']);
+
+    const atualizarSelecionado = (event) => {
+        selecionado.value = event.target.value;
+        emit('update:selecionado', selecionado.value);
+    };
+
+</script>
+
+<style scoped>
+</style>
