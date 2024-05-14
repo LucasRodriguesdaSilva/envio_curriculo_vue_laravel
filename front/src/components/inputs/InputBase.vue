91bd0dd4cfb78801f8604d03d039ce0c58c04be8
Lucas Rodrigues <lucasrodrigues@sobral.ce.gov.br>
2024-05-14 13:33:58 -0300

    feat: Adicionado componente de input base

---
+++ front/src/components/inputs/InputBase.vue
@@ -0,0 +1,28 @@
+<template>
+    <label :for="id"  class="form-label">{{titulo}}</label>
+    <input 
+        class="form-control" 
+        :type="tipo" 
+        :required="obrigatorio" 
+        :id="id" 
+        :placeholder="placeholder" 
+        :value="modelValue" 
+        @input="$emit('update:modelValue', $event.target.value)"
+        v-bind="$attrs">
+    <div v-if="obs" class="form-text">{{obs}}</div>
+</template>
+
+<script setup>
+    defineProps({
+        id: String,
+        titulo: String,
+        modelValue: { type: String, default: ''},
+        tipo: { type: String, default: 'text' },
+        obrigatorio: { type: Boolean, default: false},
+        placeholder: { type: String, default: ''},
+        obs: { type: [String, Boolean], default: false}
+    })
+</script>
+
+<style scoped>
+</style>
