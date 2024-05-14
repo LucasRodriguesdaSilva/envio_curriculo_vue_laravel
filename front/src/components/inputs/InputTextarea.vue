8b1c8d29a77b76f7d5414a1f3892e95d5b006ca4
Lucas Rodrigues <lucasrodrigues@sobral.ce.gov.br>
2024-05-14 13:34:30 -0300

    feat: Adicionado componente de texto

---
+++ front/src/components/inputs/InputTextarea.vue
@@ -0,0 +1,18 @@
+<template>
+    <label :for="id" class="form-label">{{ titulo }}</label>
+    <textarea class="form-control" :id="id" :value="modelValue" @input="$emit('update:modelValue', $event.target.value)"></textarea>
+</template>
+
+<script setup>
+    defineProps({
+        id: String,
+        titulo: String,
+        modelValue: { type: String, default: ''},
+    })
+
+
+
+</script>
+
+<style scoped>
+</style>
