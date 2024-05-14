17a5533bc0c0c82160810335af3c4635969f4152
Lucas Rodrigues <lucasrodrigues@sobral.ce.gov.br>
2024-05-14 14:56:20 -0300

    feat: Adicionado responsividade a tela

--- front/src/App.vue
+++ front/src/App.vue
@@ -3,17 +3,21 @@ import FormComponente from './components/forms/FormCurriculo.vue';
 </script>
 
 <template>
-  <main class="cor-fundo">
-    <div class="d-flex justify-content-center align-items-center" style="height: 100vh;">
-      <div class="col col-12 col-sm-6">
+  <div class="d-flex justify-content-center align-items-center mt-sm-5 mb-sm-5" >
+    <div class="col col-12 col-sm-6 cor-fundo-form">
+      <div class="p-4">
+        <h2>Se interessou pela vaga?</h2>
+        <h4>Candidate-se agora</h4>
+        <p> Preencha suas informações abaixo para submeter seu currículo e boa sorte!</p>
         <FormComponente />
       </div>
     </div>
-  </main>
+  </div>
+  
 </template>
 
 <style scoped>
-  .cor-fundo {
-    background-color: #d1bfbb
-  }
+  .cor-fundo-form {
+        background-color: #ffffff;
+    }
 </style>
