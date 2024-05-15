5dc4dd8a6a110a7b273aab821d7a5292f5ca8c7d
Lucas Rodrigues <lucasrodrigues@sobral.ce.gov.br>
2024-05-14 21:12:38 -0300

    feat: Adicionado componente de mensagem

---
+++ front/src/components/mensagens/MensagemComponente.vue
@@ -0,0 +1,13 @@
+<template>
+    <div :class="'border ' + css_bg + ' ' + ' ' + css_text">
+        <p class="p-2 m-0 text-center"> {{ msg }}</p>
+    </div>
+</template>
+
+<script setup>
+    defineProps({
+        msg: {type: String, default: ''},
+        css_text: {type: String, default: 'text-white'},
+        css_bg: {type: String, default: 'bg-danger'}
+    })
+</script>
