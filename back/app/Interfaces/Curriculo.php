5763510060037370f343cf30d7d6063a716b3582
Lucas Rodrigues <lucasrodrigues@sobral.ce.gov.br>
2024-05-15 11:47:48 -0300

    fix: Correção da interface curriculo

--- back/app/Interfaces/Curriculo.php
+++ back/app/Interfaces/Curriculo.php
@@ -2,9 +2,12 @@
 
 namespace App\Interfaces;
 
+use App\Models\Curriculo as ModelsCurriculo;
+use Illuminate\Http\JsonResponse;
 use Illuminate\Http\Request;
 
 interface Curriculo
 {
-   static public function cadastrar(Request $req);
+   static public function preparaCadastro(Request $req): JsonResponse;
+   static public function cadastrar(array $dados_validados): ModelsCurriculo;
 }
