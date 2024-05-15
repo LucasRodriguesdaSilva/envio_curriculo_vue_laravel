1103c41f68fd62f08a36aea623b8b871d0ff4f0c
Lucas Rodrigues <lucasrodrigues@sobral.ce.gov.br>
2024-05-15 11:49:11 -0300

    feat(curriculo-controller): Adicionado envio de email e refatoração

--- back/app/Http/Controllers/Curriculo.php
+++ back/app/Http/Controllers/Curriculo.php
@@ -3,22 +3,55 @@
 namespace App\Http\Controllers;
 
 use App\Interfaces\Curriculo as CurriculoInterface;
+use App\Interfaces\CurriculoMail;
+use App\Interfaces\ValidarRequest;
+use App\Mail\EnviarCurriculoRh;
 use App\Models\Curriculo as ModelsCurriculo;
+use Carbon\Carbon;
+use Illuminate\Http\JsonResponse;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Mail;
 use stdClass;
 
-class Curriculo extends Controller implements CurriculoInterface
+class Curriculo extends Controller implements CurriculoInterface, CurriculoMail, ValidarRequest
 {
 
-    static public function cadastrar(Request $req)
+    static function preparaCadastro(Request $req): JsonResponse
     {
-        $validateData = self::validar($req);
-        $validateData['caminho_anexo'] = $req->file('curriculo')->store('curriculos');
-        ModelsCurriculo::create($validateData);        
+        $dados_validados = self::validar($req);
+
+        $dados_validados['caminho_anexo'] = $req->file('curriculo')->store('curriculos');
+        $dados_validados['tipo_anexo'] =  $req->file('curriculo')->getMimeType();
+        $dados_validados['ip'] = $req->ip();
+        
+        $modelsCurriculo = self::cadastrar($dados_validados);
+        self::enviarEmail($modelsCurriculo);       
+ 
         return response()->json('success');
     }
 
-    static private function validar(Request $req)
+    static function cadastrar(array $dadosValidados): ModelsCurriculo
+    {
+        return ModelsCurriculo::create($dadosValidados);
+    }
+
+    static function enviarEmail(ModelsCurriculo $modelsCurriculo)
+    {
+        Mail::to('destinatario@example.com')
+            ->send(new EnviarCurriculoRh($modelsCurriculo));
+
+        self::cadastrarDataHoraEnvioEmail($modelsCurriculo);
+
+        return true;
+    }
+
+    static private function cadastrarDataHoraEnvioEmail(ModelsCurriculo $modelsCurriculo)
+    {
+        $modelsCurriculo->data_hora_envio_email = Carbon::now();
+        $modelsCurriculo->save();
+    }
+
+    static function validar(Request $req): Array
     {
         return $req->validate([
             'candidato' => 'required|string|max:255',
@@ -28,6 +61,7 @@ class Curriculo extends Controller implements CurriculoInterface
             'escolaridade' => 'required|string',
             'obs' => 'nullable|string',
             'nome_anexo' => 'required|string',
+            'curriculo' => 'required|file|mimes:pdf,doc,docx|max:1054'
         ]);
                 
     }
