43582b253d22d21412e9477b06eb63982d46f795
Lucas Rodrigues <lucasrodrigues@sobral.ce.gov.br>
2024-05-14 21:50:29 -0300

    feat: Adicionado endpoint para o formulário

--- front/src/App.vue
+++ front/src/App.vue
@@ -1,15 +1,17 @@
 <script setup>
-import FormComponente from './components/forms/FormCurriculo.vue';
+
+  import { ref } from 'vue';
+  import FormComponente from './components/forms/FormCurriculo.vue';
+
+  const base = ref('http://127.0.0.1:8000/api/');
+  const endpoint = ref('curriculo/cadastrar');
 </script>
 
 <template>
   <div class="d-flex justify-content-center align-items-center mt-sm-5 mb-sm-5" >
     <div class="col col-12 col-sm-6 cor-fundo-form">
       <div class="p-4">
-        <h2>Se interessou pela vaga?</h2>
-        <h4>Candidate-se agora</h4>
-        <p> Preencha suas informações abaixo para submeter seu currículo e boa sorte!</p>
-        <FormComponente />
+        <FormComponente :endpoint="base + endpoint" />
       </div>
     </div>
   </div>
